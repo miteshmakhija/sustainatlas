c22e1bd0098430ff5fbdf340047a0b1ae1dfbbc9
Mitesh Makhija <miteshmakhija@gmail.com>
2026-06-20 13:22:11 +0530

    Moved to https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
-    port: 5173,
+    port: 8080,
+    https: true,
     proxy: {
       // Forward API calls to the Python (FastAPI) backend during dev
       '/api': {
